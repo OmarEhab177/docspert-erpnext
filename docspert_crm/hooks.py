90a00db4cb5d0855a2824d12257d5116691c267e
erpnext <erpnext@ip-172-31-34-239.eu-central-1.compute.internal>
2022-09-18 13:55:28 +0000

    remove fixture from custom app

--- docspert_crm/hooks.py
+++ docspert_crm/hooks.py
@@ -10,7 +10,7 @@ app_license = "MIT"
 # Includes in <head>
 # ------------------
 
-fixtures = ['DocType']
+# fixtures = ['DocType']
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/docspert_crm/css/docspert_crm.css"
