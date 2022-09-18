a6f0216c894bde0a7a8be54f2b62572975c39cbe
OmarEhab177 <omarehap17@gmail.com>
2022-09-18 05:24:05 +0200

    enter developer mode and uncheck custom from doctype

---
+++ docspert_crm/docspert_crm/doctype/docspertlead/docspertlead.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, omar and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class DocspertLead(Document):
+	pass
